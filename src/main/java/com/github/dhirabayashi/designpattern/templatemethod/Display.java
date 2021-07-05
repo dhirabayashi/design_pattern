6f7c99f0aa29615ee2c53a5840328728dba6c7e0
daiki <gigabowser@hotmail.co.jp>
2021-07-05 19:42:05 +0900

    templateMethod初期コミット

---
+++ src/main/java/com/github/dhirabayashi/designpattern/templatemethod/Display.java
@@ -0,0 +1,9 @@
+package com.github.dhirabayashi.designpattern.templatemethod;
+
+public abstract class Display {
+    abstract void open();
+    abstract void print();
+    abstract void close();
+
+
+}
---
+++ src/main/java/com/github/dhirabayashi/designpattern/templatemethod/StringDisplay.java
@@ -0,0 +1,4 @@
+package com.github.dhirabayashi.designpattern.templatemethod;
+
+public class StringDisplay {
+}
