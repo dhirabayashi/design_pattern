f5ff68922ce396806f6fd2e004c7a606dadb6418
daiki <gigabowser@hotmail.co.jp>
2021-08-19 13:39:45 +0900

    Facade初期コミット

---
+++ src/main/java/com/github/dhirabayashi/designpattern/facade/Main.java
@@ -0,0 +1,4 @@
+package com.github.dhirabayashi.designpattern.facade;
+
+public class Main {
+}
---
+++ src/main/java/com/github/dhirabayashi/designpattern/facade/pagemaker/HtmlWriter.java
@@ -0,0 +1,4 @@
+package com.github.dhirabayashi.designpattern.facade.pagemaker;
+
+public class HtmlWriter {
+}
