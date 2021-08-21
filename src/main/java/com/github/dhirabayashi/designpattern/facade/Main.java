7282e3cf6fd23fb9f7cb81b6bc629b3f0f53d0ea
daiki <gigabowser@hotmail.co.jp>
2021-08-21 12:09:27 +0900

    Facade課題実装

--- src/main/java/com/github/dhirabayashi/designpattern/facade/Main.java
+++ src/main/java/com/github/dhirabayashi/designpattern/facade/Main.java
@@ -5,5 +5,6 @@ import com.github.dhirabayashi.designpattern.facade.pagemaker.PageMaker;
 public class Main {
     public static void main(String[] args) {
         PageMaker.makeWelcomePage("hyuki@hyuki.com", "welcome.html");
+        PageMaker.makeLinkPage("linkpage.html");
     }
 }
--- src/main/java/com/github/dhirabayashi/designpattern/facade/pagemaker/PageMaker.java
+++ src/main/java/com/github/dhirabayashi/designpattern/facade/pagemaker/PageMaker.java
@@ -2,7 +2,7 @@ package com.github.dhirabayashi.designpattern.facade.pagemaker;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Properties;
+import java.io.UncheckedIOException;
 
 public class PageMaker {
     private PageMaker() {}
@@ -16,6 +16,23 @@ public class PageMaker {
             writer.paragraph("メールまっていますね。");
             writer.mailto(mailAddress, username);
             System.out.println(filename + " is created for " + mailAddress + " (" + username + ")");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void makeLinkPage(String filename) {
+        try(var writer = new HtmlWriter(new FileWriter(filename))) {
+            var mailProp = Database.getProperties("maildata");
+            writer.title("Link page");
+            mailProp.forEach((mailAddress, username) -> {
+                try {
+                    writer.mailto((String)mailAddress, (String)username);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            });
+            System.out.println(filename + " is created.");
         } catch (IOException e) {
             e.printStackTrace();
         }
