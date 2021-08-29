9319db039f2b7d5e8172421e57f2d75de71474f9
daiki <gigabowser@hotmail.co.jp>
2021-08-29 21:54:56 +0900

    Memento実装

--- src/main/java/com/github/dhirabayashi/designpattern/memento/game/Gamer.kt
+++ src/main/java/com/github/dhirabayashi/designpattern/memento/game/Gamer.kt
@@ -1,4 +1,64 @@
 package com.github.dhirabayashi.designpattern.memento.game
 
-class Gamer {
+import java.util.concurrent.ThreadLocalRandom
+
+class Gamer(private var money: Int) {
+    private var fruits = mutableListOf<String>()
+    private val random = ThreadLocalRandom.current()
+
+    companion object {
+        val fruitsName = arrayOf("りんご", "ぶどう", "バナナ", "みかん")
+    }
+
+    fun getMoney(): Int {
+        return money
+    }
+
+    fun bet() {
+        when (random.nextInt(6) + 1) {
+            1 -> {
+                money += 100
+                println("所持金が増えました。")
+            }
+            2 -> {
+                money /= 2
+                println("所持金が半分になりました。")
+            }
+            6 -> {
+                val f = getFruit()
+                println("フルーツ($f)をもらいました。")
+                fruits.add(f)
+            }
+            else -> {
+                println("何も起こりませんでした。")
+            }
+        }
+    }
+
+    fun createMemento(): Memento {
+        val m = Memento(money)
+        for(f in fruits) {
+            if(f.startsWith("おいしい")) {
+                m.addFruits(f)
+            }
+        }
+        return m
+    }
+
+    fun restoreMemento(memento: Memento) {
+        this.money = memento.getMoney()
+        this.fruits = memento.getFruits()
+    }
+
+    override fun toString(): String {
+        return "[money = $money], fruits = $fruits]"
+    }
+
+    private fun getFruit(): String {
+        var prefix = ""
+        if(random.nextBoolean()) {
+            prefix += "おいしい"
+        }
+        return prefix + fruitsName[random.nextInt(fruitsName.size)]
+    }
 }
--- src/main/java/com/github/dhirabayashi/designpattern/memento/game/Memento.kt
+++ src/main/java/com/github/dhirabayashi/designpattern/memento/game/Memento.kt
@@ -1,4 +1,17 @@
 package com.github.dhirabayashi.designpattern.memento.game
 
-class Memento {
+class Memento(private val money: Int) {
+    private val fruits = mutableListOf<String>()
+
+    fun getMoney(): Int {
+        return money
+    }
+
+    internal fun addFruits(fruit: String) {
+        fruits.add(fruit)
+    }
+
+    internal fun getFruits(): MutableList<String> {
+        return fruits.toMutableList()
+    }
 }
